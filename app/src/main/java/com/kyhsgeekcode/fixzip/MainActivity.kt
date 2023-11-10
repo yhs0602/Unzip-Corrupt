5d23ae685bc002532d1487205ada46442be48040
yhs0602 <jourhyang123@gmail.com>
2023-11-10 22:53:45 +0900

    :up-arrow: android 33

--- app/src/main/java/com/kyhsgeekcode/fixzip/MainActivity.kt
+++ app/src/main/java/com/kyhsgeekcode/fixzip/MainActivity.kt
@@ -1,30 +1,31 @@
 package com.kyhsgeekcode.fixzip
 
+//import com.google.android.play.core.assetpacks.*
+//import com.google.android.play.core.assetpacks.model.AssetPackStatus
+//import com.google.android.play.core.tasks.RuntimeExecutionException
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
-import android.widget.*
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
-import com.google.android.play.core.assetpacks.*
-import com.google.android.play.core.assetpacks.model.AssetPackStatus
-import com.google.android.play.core.tasks.RuntimeExecutionException
 import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.launch
 import timber.log.Timber
-import timber.log.Timber.*
-import java.io.File
-import java.util.*
+import timber.log.Timber.Tree
 
 
 class MainActivity : AppCompatActivity(), View.OnClickListener, IConsole {
-    private lateinit var assetPackManager: AssetPackManager
-    private var waitForWifiConfirmationShown = false
-    private val packName: String by lazy {
-        packNameByDensity()
-    }
+//    private lateinit var assetPackManager: AssetPackManager
+//    private var waitForWifiConfirmationShown = false
+//    private val packName: String by lazy {
+//        packNameByDensity()
+//    }
 
     override fun print(s: String?) {
         runOnUiThread {
@@ -65,129 +66,130 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, IConsole {
         // Handle item selection
         return when (item.itemId) {
             R.id.menu_download -> {
-                downloadResources()
+//                downloadResources()
                 true
             }
+
             else -> super.onOptionsItemSelected(item)
         }
     }
 
-    private fun downloadResources() {
-        Timber.d("Downloading resources")
-        initAssetPackManager()
-        loadAssets()
-    }
+//    private fun downloadResources() {
+//        Timber.d("Downloading resources")
+////        initAssetPackManager()
+////        loadAssets()
+//    }
 
-    private fun initAssetPackManager() {
-        assetPackManager = AssetPackManagerFactory.getInstance(applicationContext)
-    }
+//    private fun initAssetPackManager() {
+//        assetPackManager = AssetPackManagerFactory.getInstance(applicationContext)
+//    }
 
-    private fun loadAssets() {
-        val onDemandAssetPackPath: String? =
-            assetPackManager.getAbsoluteAssetPath(packName, "test.txt")
-        if (onDemandAssetPackPath == null) {
-            Timber.d(" OnDemand AssetPath : Null; fetching packname: $packName")
-            assetPackManager.registerListener(assetPackStateUpdateListener)
-            assetPackManager.fetch(listOf(packName)).addOnCompleteListener {
-                if (it.isSuccessful) {
-                    Timber.d("Oncomplete Success" + it.result.packStates().keys)
-                } else {
-                    Timber.d("OnComplete faild fetch " + it.exception)
-                }
-            }
-            Timber.d(" Called fetch")
-        } else {
-            // ready. load
-            Timber.d(" Ready. load")
-            initWithLoadedAssets(onDemandAssetPackPath)
-        }
-    }
+//    private fun loadAssets() {
+//        val onDemandAssetPackPath: String? =
+//            assetPackManager.getAbsoluteAssetPath(packName, "test.txt")
+//        if (onDemandAssetPackPath == null) {
+//            Timber.d(" OnDemand AssetPath : Null; fetching packname: $packName")
+//            assetPackManager.registerListener(assetPackStateUpdateListener)
+//            assetPackManager.fetch(listOf(packName)).addOnCompleteListener {
+//                if (it.isSuccessful) {
+//                    Timber.d("Oncomplete Success" + it.result.packStates().keys)
+//                } else {
+//                    Timber.d("OnComplete faild fetch " + it.exception)
+//                }
+//            }
+//            Timber.d(" Called fetch")
+//        } else {
+//            // ready. load
+//            Timber.d(" Ready. load")
+//            initWithLoadedAssets(onDemandAssetPackPath)
+//        }
+//    }
 
-    private fun initWithLoadedAssets(path: String) {
-        val file = File(path)
-        Timber.d("Init with loaded assets. Path: $path, file: $file")
-        val helpText = file.readText(Charsets.UTF_8)
-        print(helpText)
-    }
+//    private fun initWithLoadedAssets(path: String) {
+//        val file = File(path)
+//        Timber.d("Init with loaded assets. Path: $path, file: $file")
+//        val helpText = file.readText(Charsets.UTF_8)
+//        print(helpText)
+//    }
 
-    private var assetPackStateUpdateListener =
-        AssetPackStateUpdateListener { state ->
-            when (state.status()) {
-                AssetPackStatus.PENDING -> Timber.i("Pending")
-                AssetPackStatus.DOWNLOADING -> {
-                    val downloaded = state.bytesDownloaded()
-                    val totalSize = state.totalBytesToDownload()
-                    val percent = 100.0 * downloaded / totalSize
-                    Timber.i("PercentDone=" + String.format("%.2f", percent))
-                }
-                AssetPackStatus.TRANSFERRING -> {
-                    Timber.d("Transferrring")
-                }
-                AssetPackStatus.COMPLETED -> {
-                    Timber.d("Completed")
-                    loadAssets()
+//    private var assetPackStateUpdateListener =
+//        AssetPackStateUpdateListener { state ->
+//            when (state.status()) {
+//                AssetPackStatus.PENDING -> Timber.i("Pending")
+//                AssetPackStatus.DOWNLOADING -> {
+//                    val downloaded = state.bytesDownloaded()
+//                    val totalSize = state.totalBytesToDownload()
+//                    val percent = 100.0 * downloaded / totalSize
+//                    Timber.i("PercentDone=" + String.format("%.2f", percent))
+//                }
+//                AssetPackStatus.TRANSFERRING -> {
+//                    Timber.d("Transferrring")
+//                }
+//                AssetPackStatus.COMPLETED -> {
+//                    Timber.d("Completed")
+//                    loadAssets()
+//
+//                }                    // Asset pack is ready to use. Start the Game/App.
+//                AssetPackStatus.FAILED -> {                  // Request failed. Notify user.
+//                    Timber.d("Failed" + state.errorCode().toString())
+//
+//                }
+//                AssetPackStatus.CANCELED -> {
+//                    Timber.d("Canceled")
+//                }
+//                AssetPackStatus.WAITING_FOR_WIFI -> showWifiConfirmationDialog()
+//                AssetPackStatus.NOT_INSTALLED -> {
+//                    Timber.d("Not installed")
+//                }
+//                AssetPackStatus.UNKNOWN -> {
+//                    Timber.d("Unknown")
+//                }
+//            }
+//        }
 
-                }                    // Asset pack is ready to use. Start the Game/App.
-                AssetPackStatus.FAILED -> {                  // Request failed. Notify user.
-                    Timber.d("Failed" + state.errorCode().toString())
+//    private fun AssetPackManager.loadAssetByName(assetPackName: String) {
+//        getPackStates(Collections.singletonList(assetPackName))
+//            .addOnCompleteListener {
+//                val assetPackStates: AssetPackStates
+//                try {
+//                    assetPackStates = it.result
+//                    val assetPackState: AssetPackState? =
+//                        assetPackStates.packStates()[assetPackName]
+//                } catch (e: RuntimeExecutionException) {
+//                    Timber.e(e, "Failed to get pack states")
+//                }
+//            }
+//    }
 
-                }
-                AssetPackStatus.CANCELED -> {
-                    Timber.d("Canceled")
-                }
-                AssetPackStatus.WAITING_FOR_WIFI -> showWifiConfirmationDialog()
-                AssetPackStatus.NOT_INSTALLED -> {
-                    Timber.d("Not installed")
-                }
-                AssetPackStatus.UNKNOWN -> {
-                    Timber.d("Unknown")
-                }
-            }
-        }
+//    private fun showWifiConfirmationDialog() {
+//        if (!waitForWifiConfirmationShown) {
+//            assetPackManager.showCellularDataConfirmation(this@MainActivity)
+//                .addOnSuccessListener { resultCode ->
+//                    if (resultCode == RESULT_OK) {
+//                        Timber.d("Confirmation dialog has been accepted.")
+//                        loadAssets()
+//                    } else if (resultCode == RESULT_CANCELED) {
+//                        Timber.d("Confirmation dialog has been denied by the user.")
+//                        Toast.makeText(this,
+//                            "Please Connect to Wifi to begin app files to download",
+//                            Toast.LENGTH_SHORT).show()
+//                    }
+//                }
+//            waitForWifiConfirmationShown = true
+//        }
+//    }
 
-    private fun AssetPackManager.loadAssetByName(assetPackName: String) {
-        getPackStates(Collections.singletonList(assetPackName))
-            .addOnCompleteListener {
-                val assetPackStates: AssetPackStates
-                try {
-                    assetPackStates = it.result
-                    val assetPackState: AssetPackState? =
-                        assetPackStates.packStates()[assetPackName]
-                } catch (e: RuntimeExecutionException) {
-                    Timber.e(e, "Failed to get pack states")
-                }
-            }
-    }
-
-    private fun showWifiConfirmationDialog() {
-        if (!waitForWifiConfirmationShown) {
-            assetPackManager.showCellularDataConfirmation(this@MainActivity)
-                .addOnSuccessListener { resultCode ->
-                    if (resultCode == RESULT_OK) {
-                        Timber.d("Confirmation dialog has been accepted.")
-                        loadAssets()
-                    } else if (resultCode == RESULT_CANCELED) {
-                        Timber.d("Confirmation dialog has been denied by the user.")
-                        Toast.makeText(this,
-                            "Please Connect to Wifi to begin app files to download",
-                            Toast.LENGTH_SHORT).show()
-                    }
-                }
-            waitForWifiConfirmationShown = true
-        }
-    }
-
-    private fun AssetPackManager.getAbsoluteAssetPath(
-        assetPack: String,
-        relativeAssetPath: String,
-    ): String? {
-        val assetPackPath: AssetPackLocation = getPackLocation(assetPack)
-            ?: // asset pack is not ready
-            return null
-        val assetsFolderPath: String = assetPackPath.assetsPath() ?: return null
-        // equivalent to: FilenameUtils.concat(assetPackPath.path(), "assets");
-        return File(File(assetsFolderPath), relativeAssetPath).absolutePath
-    }
+//    private fun AssetPackManager.getAbsoluteAssetPath(
+//        assetPack: String,
+//        relativeAssetPath: String,
+//    ): String? {
+//        val assetPackPath: AssetPackLocation = getPackLocation(assetPack)
+//            ?: // asset pack is not ready
+//            return null
+//        val assetsFolderPath: String = assetPackPath.assetsPath() ?: return null
+//        // equivalent to: FilenameUtils.concat(assetPackPath.path(), "assets");
+//        return File(File(assetsFolderPath), relativeAssetPath).absolutePath
+//    }
 
     lateinit var btGo: Button
     lateinit var lvScreen: ListView
@@ -230,19 +232,19 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, IConsole {
         }
     }
 
-    private fun packNameByDensity() = "drawable_${getDensity()}"
+//    private fun packNameByDensity() = "drawable_${getDensity()}"
 
-    private fun getDensity(): String {
-        return "hdpi"
-        val d = resources.displayMetrics.density
-
-        return when {
-            d < 1.0f -> "ldpi"
-            d < 1.5f -> "mdpi"
-            d < 2.0f -> "hdpi"
-            d < 3.0f -> "xhdpi"
-            d < 4.0f -> "xxhdpi"
-            else -> "xxxhdpi"
-        }
-    }
+//    private fun getDensity(): String {
+//        return "hdpi"
+//        val d = resources.displayMetrics.density
+//
+//        return when {
+//            d < 1.0f -> "ldpi"
+//            d < 1.5f -> "mdpi"
+//            d < 2.0f -> "hdpi"
+//            d < 3.0f -> "xhdpi"
+//            d < 4.0f -> "xxhdpi"
+//            else -> "xxxhdpi"
+//        }
+//    }
 }
